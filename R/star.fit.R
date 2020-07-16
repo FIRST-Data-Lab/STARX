eff50c3521ab427396e170392f63e70d43bcb325
Shan Yu <syu@iastate.edu>
2020-07-16 14:18:40 -0500

    add splines2

--- R/star.fit.R
+++ R/star.fit.R
@@ -3,7 +3,7 @@
 #' This function fits a spatiotemporal autoregressive (STAR) model
 #' with exogenous variables.
 #'
-#' @import TPST Matrix MGLM
+#' @import TPST Matrix MGLM splines2
 #' @param data A list of five containing the data to fit model. \code{Y} is the
 #' response variable. \code{Z} is exogenous variables with constant linear
 #' coefficients. \code{X} is exogenous variables with varying linear
